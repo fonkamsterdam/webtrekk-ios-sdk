643a5ec7173e05e7ec5af429249fa68c3c2b8906
Bart Schoon <bart@fonk.amsterdam>
2021-01-04 14:12:31 +0100

    Updated Swift tool version

--- Package.swift
+++ Package.swift
@@ -1,17 +1,20 @@
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "Webtrekk",
     platforms: [
-        .iOS(.v10),
-        .macOS(.v10_11),
-        .tvOS(.v10)
+        .iOS(.v10)
     ],
     products: [
         .library(name: "Webtrekk", targets: ["Webtrekk"]),
     ],
     targets: [
-        .target(name: "Webtrekk", path: "Source/**/*.swift")
+        .target(name: "Webtrekk",
+                path: "Source/**/*.swift"),
+                exclude:"Source/Internal/Utility/WKInterfaceController.swift"
     ],
     swiftLanguageVersions: [
         .v5
