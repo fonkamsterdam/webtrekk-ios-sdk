1e9a57f48aed103eded87cf387d27e929f441812
Bart Schoon <bart@fonk.amsterdam>
2021-01-04 13:52:54 +0100

    Updated Sources

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .library(name: "Webtrekk", targets: ["Webtrekk"]),
     ],
     targets: [
-        .target(name: "Webtrekk", path: "Sources")
+        .target(name: "Webtrekk", path: "Source/**/*.swift")
     ],
     swiftLanguageVersions: [
         .v5
